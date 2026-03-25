8ffa513490517e41879408058e2a93c41eb9003b
Shaurya Singh <shauryasingh8d@gmail.com>
2026-03-25 09:48:51 +0530

    Refactor name assignment in HelloApp

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,6 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = (args.length > 0) ? args[0] : "World";
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
         System.out.println("Hello, " + name + "!");
     }
 }
