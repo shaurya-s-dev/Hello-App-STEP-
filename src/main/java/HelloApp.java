57a8fcba13105bb785e9b204b9915a6c43a6c115
Shaurya Singh <shauryasingh8d@gmail.com>
2026-03-26 08:51:16 +0530

    Simplify greeting logic in HelloApp

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,6 @@
 public class HelloApp {
-
     public static void main(String[] args) {
-
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
-
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
